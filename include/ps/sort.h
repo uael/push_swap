375640325c02cc98ef459e5222c131e20d9abe9e
uael <lucas.abel@yandex.com>
2018-03-20 16:56:34 +0100

    added some doc

--- include/ps/sort.h
+++ include/ps/sort.h
@@ -17,12 +17,39 @@
 
 struct s_ps;
 
+/*
+** Sort the `stack`.
+** @param ps    The ps env
+** @param stack The id of stack to sort
+** @param n     The size to sort
+*/
 extern void	ps_sort(struct s_ps *ps, uint8_t stack, uint32_t n);
 
+/*
+** Sort the `stack` using the quick sort algorithm.
+** Sort from 45 to INF
+** @param ps    The ps env
+** @param stack The id of stack to sort
+** @param n     The size to sort
+*/
 extern void	ps_quicksort(struct s_ps *ps, uint8_t stack, uint32_t n);
 
+/*
+** Sort the `stack` using the selection sort algorithm.
+** Sort from 3 to 45.
+** @param ps    The ps env
+** @param stack The id of stack to sort
+** @param n     The size to sort
+*/
 extern void	ps_selectsort(struct s_ps *ps, uint8_t stack);
 
+/*
+** Sort the `stack` using custom algorithm.
+** Sort from 0 to 3.
+** @param ps    The ps env
+** @param stack The id of stack to sort
+** @param n     The size to sort
+*/
 extern void	ps_minisort(struct s_ps *ps, uint8_t stack, uint32_t n);
 
 #endif
